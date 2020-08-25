066969f7d37af13b77e261a2dab85a40206c7aa0
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-08-25 18:28:01 -0400

    save before deploy

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// const mongoose = require('mongoose')
+
+// const dbOnes = mongoose.createConnection(process.env./*--your typeone MongoDB URI name from .env---*/, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+
+// dbOnes.on('connected', () => console.log(`connected to MongoDB Ones at ${dbOnes.host}:${dbOnes.port}`))
+
+// const dbTwos = mongoose.createConnection(process.env./*--your typetwo MongoDB URI name from .env---*/, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+
+// dbTwos.on('connected', () => console.log(`connected to MongoDB Twos at ${dbTwos.host}:${dbTwos.port}`))
---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+const logger = require('morgan')
+const methodOverride = require('method-override')
+const cookieParser = require('cookie-parser')
+const httpErrors = require('http-errors')
+// require('dotenv').config()
+// require('./config/database')
+const indexRouter = require('./routes/index')
+const onesRouter = require('./routes/typeones')
+// const twosRouter = require('./routes/typetwos')
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use(logger('dev'))
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(methodOverride('_method'))
+
+app.use('/', indexRouter)
+app.use('/typeones', onesRouter)
+// app.use('/typetwos', twosRouter)
+
+app.use((req, res, next) => next(httpErrors(404)))
+app.use((err, req, res, next) => {
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.status(err.status || 500)
+    res.render('error')
+})
+
+module.exports = app
