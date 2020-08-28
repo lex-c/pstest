f35c1454bbf2d068ef7ae4eaf13ac8f8f2587852
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-08-28 14:10:22 -0400

    asf

--- controllers/typeones.js
+++ controllers/typeones.js
@@ -2,6 +2,6 @@
 module.exports = {onesIndex}
 
 function onesIndex(req, res) {
-    if (!req.user) res.render('typeones/index', {title: 'Ones'})
-    res.render('typeones/index', {title: 'Ones', name: req.user.name, user: req.user})
+    if (!req.user) res.render('typeones/index', {title: 'Ones' , ip: req.connection.remoteAddress})
+    res.render('typeones/index', {title: 'Ones', name: req.user.name, user: req.user, ip: req.connection.remoteAddress})
 }
--- models/user.js
+++ models/user.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const picsIntSchema = new Schema({
+    picObjId: String,
+    int: Number
+})
+
 const userSchema = new Schema({
     name: String,
     email: String,
-    googleId: String
+    googleId: String,
+    ipAdd: String,
+    picsInt: [picsIntSchema]
 }, {timestamps: true})
 
 module.exports = mongoose.model('User', userSchema)
