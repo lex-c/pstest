ffe770ece4fa8902d134e54f9cd91ceade4fcfe1
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-08-27 22:25:16 -0400

    sdf

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+const p1CD = document.getElementById('p1Count')
+const p2CD = document.getElementById('p2Count')
+const p3CD = document.getElementById('p3Count')
+const p4CD = document.getElementById('p4Count')
+const p5CD = document.getElementById('p5Count')
+const p6CD = document.getElementById('p6Count')
+
+let p1Ct, p2Ct, p3Ct, p4Ct, p5Ct, p6Ct = 0
+
