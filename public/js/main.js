7bd537b2a112595fa6b6820928295dd6333df62f
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-08-27 23:29:04 -0400

    dasfd

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,12 @@ const p3CD = document.getElementById('p3Count')
 const p4CD = document.getElementById('p4Count')
 const p5CD = document.getElementById('p5Count')
 const p6CD = document.getElementById('p6Count')
-const pic1 = document.getElementById('pic1')
-const pic2 = document.getElementById('pic2')
-const pic3 = document.getElementById('pic3')
-const pic4 = document.getElementById('pic4')
-const pic5 = document.getElementById('pic5')
-const pic6 = document.getElementById('pic6')
+var pic1 = document.getElementById('pic1')
+var pic2 = document.getElementById('pic2')
+var pic3 = document.getElementById('pic3')
+var pic4 = document.getElementById('pic4')
+var pic5 = document.getElementById('pic5')
+var pic6 = document.getElementById('pic6')
 
 
 let p1Ct = p2Ct = p3Ct = p4Ct = p5Ct = p6Ct = 0
---
+++ test.js
@@ -0,0 +1,10 @@
+// if (pic1.id === 'pic1') {
+//     pic1.id = 'pic1b'
+//     pic1 = document.getElementById('pic1b')
+//     pic1.offsetHeight
+// }
+// if (pic1.id === 'pic1b') {
+//     pic1.id = 'pic1'
+//     pic1 = document.getElementById('pic1b')
+//     pic1.offsetHeight
+// }
