318ad561c683b010ee96cbd4047e64e4ea96599d
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-08-29 03:48:43 -0400

    add DB storage of intNum

--- models/user.js
+++ models/user.js
@@ -2,16 +2,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const picsIntSchema = new Schema({
-    picObjId: String,
-    int: Number
+    picId: String,
+    intNum: Number
 })
 
+const picsRotSchema = new Schema({
+    picId: String,
+    intNum: {type: Number, default: 0}
+    // srcUrl: String
+})
 const userSchema = new Schema({
     name: String,
     email: String,
     googleId: String,
     ipAdd: String,
-    picsInt: [picsIntSchema]
+    picsInt: [picsIntSchema],
+    picsRot: [picsRotSchema]
 }, {timestamps: true})
 
 module.exports = mongoose.model('User', userSchema)
