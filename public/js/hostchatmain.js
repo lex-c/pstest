6ce5de529ec0567314fe0d44f901bbabd10fd396
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-09-04 10:18:08 -0400

    fix hostchat

--- public/js/hostchatmain.js
+++ public/js/hostchatmain.js
@@ -1,10 +1,10 @@
 const reqD = document.querySelector('.requestdiv')
 let name
-const showRequest = (nameAndIntsAndIp) => {
-    name = nameAndInts[0] ? nameAndInts[0] : `Someone who hasn't given their name`
+const showRequest = (nameIntsIp) => {
+    name = nameIntsIp[0] ? nameIntsIp[0] : `Someone who hasn't given their name`
     reqD.innerHTML =
         `${name} is asking to chat with you; 
-        they like:${nameAndIntsAndIp[1].map(int => ` ${int}`)}
-        accept? <a href="/chat/host/${nameAndIntsAndIp[2]}">yes</a>`
+        they like:${nameIntsIp[1].map(int => ` ${int}`)}
+        accept? <a href="/chat/host/${nameIntsIp[2]}">yes</a>`
     reqD.style.setProperty('display', 'inherit')
 }
