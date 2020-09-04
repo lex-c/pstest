dced2ce0b587146804cfa61555bf51832ee86e64
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-09-04 10:42:07 -0400

    add

--- public/js/hostchatmain.js
+++ public/js/hostchatmain.js
@@ -6,7 +6,7 @@ let name, intsMess
 const showRequest = (nameIntsIp) => {
     console.log('getting the request')
     name = nameIntsIp[0] ? nameIntsIp[0] : `Someone who hasn't given their name`
-    intsMess = nameIntsIp[1].length) ? nameIntsIp[1].join(' ') : 'nothing added yet'
+    intsMess = nameIntsIp[1].length ? nameIntsIp[1].join(' ') : 'nothing added yet'
     reqD.innerHTML =
         `${name} is asking to chat with you; 
         Their interests: ${intsMess}
