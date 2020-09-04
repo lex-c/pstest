343e0a37d8ddf46633e388635e7674628ba0d43b
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-09-04 13:06:18 -0400

    add

--- routes/chat.js
+++ routes/chat.js
@@ -9,5 +9,7 @@ router.post('/:userIp/messages', chatCtrlr.addMess)
 router.post('/:userIp/messages/host', chatCtrlr.hostAddMess)
 router.get('/:userIp/:isAdd/:isMe', chatCtrlr.chatPage)
 router.get('/host/:userIp/:isAdd/:isMe', chatCtrlr.hostPage)
+router.get('/:userIp/:isAdd', chatCtrlr.chatPage)
+router.get('/host/:userIp/:isAdd', chatCtrlr.hostPage)
 
 module.exports = router
