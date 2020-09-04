90050b4f8b3eff472e3e55fc09dc12c29b0c52f6
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-09-04 10:41:32 -0400

    add

--- public/js/hostchatmain.js
+++ public/js/hostchatmain.js
@@ -1,11 +1,15 @@
+const user = require("../../models/user")
+
+
 const reqD = document.querySelector('.requestdiv')
-let name
+let name, intsMess
 const showRequest = (nameIntsIp) => {
     console.log('getting the request')
     name = nameIntsIp[0] ? nameIntsIp[0] : `Someone who hasn't given their name`
+    intsMess = nameIntsIp[1].length) ? nameIntsIp[1].join(' ') : 'nothing added yet'
     reqD.innerHTML =
         `${name} is asking to chat with you; 
-        they like:${nameIntsIp[1].map(int => ` ${int}`)}
+        Their interests: ${intsMess}
         accept? <a href="/chat/host/${nameIntsIp[2]}">yes</a>`
     reqD.style.setProperty('display', 'inherit')
     reqD.offsetHeight
