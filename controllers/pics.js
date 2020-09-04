8681c8de63c52ebdbcab4aa9314ecf68f56ca1f5
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-09-04 02:35:18 -0400

    sd

--- controllers/pics.js
+++ controllers/pics.js
@@ -5,7 +5,14 @@ function picsIndex(req, res) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
     User.findOne({ipAdd: ip}, (err, ipUser) => {
         if (!ipUser) return res.render('pics/index', {title: 'All Pics', ip})
-        if (ipUser && req.user || ipUser && req.query.user) res.render('pics/index', {title: 'All Pics', user: (req.user || JSON.parse(req.query.user)), ip})
+        if (ipUser && req.user || ipUser && req.query.userIp) {
+            if (ipUser && req.user) res.render('pics/index', {title: 'All Pics', user: req.user, ip})
+            if (ipUser && req.query.userIp) {
+                User.findOne({ipAdd: req.query.userIp}, (err, userFnd) => {
+                    res.render('pics/index', {title: 'All Pics', user: userFnd, ip: req.query.userIp})
+                })
+            }
+        }
         if (ipUser && ipUser.googleId) return res.render('pics/index', {title: 'All Pics', ipUser, ip})
         res.render('pics/index', {title: 'All Pics', ints: ipUser.intTags, ip})
     })
